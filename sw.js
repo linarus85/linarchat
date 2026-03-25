ddc1db5955891ed67fc7b37659b61c6106219338
linarus85 <99500209+linarus85@users.noreply.github.com>
2026-03-25 15:48:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,11 @@
 const CACHE_NAME = 'linarchat-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json'
-];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-    );
     self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+    event.respondWith(fetch(event.request));
 });
 
 self.addEventListener('activate', event => {
